befe16a097e9a34a24b69395171070ee49352eb8
root <root@localhost.localdomain>
2020-07-14 07:03:03 +0000

    初始化

---
+++ mp/app/Http/Controllers/User/IndixController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class IndixController extends Controller
+{
+    //
+    public function reg(){
+        echo '123';
+    }
+}
